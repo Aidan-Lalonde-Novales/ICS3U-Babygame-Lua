4dec11592e898e85ff39904a8bc069f7137d2cb3
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-06-23 03:45:59 +0000

    cool!

---
+++ Babygame/Babygame1/main.lua
@@ -0,0 +1,44 @@
+-- 1st Baby Game Learning Guide
+
+-- adds font, resize, title change, esc to quit, and fps counter
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load()  
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true
+    })
+
+    love.window.setTitle('Baby Game by A.M.W.L.N')
+
+    smallfont = love.graphics.newFont('fonts/pixel_text.ttf', 14)
+    largefont = love.graphics.newFont('fonts/pixel_text.ttf', 32)
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.draw()
+    displayFPS()
+    love.graphics.setFont(largefont)
+    love.graphics.printf("Hello World!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function displayFPS()
+    love.graphics.setColor(254 / 255, 255 / 255, 254 / 255, 255 / 255)
+    love.graphics.setFont(smallfont)
+    love.graphics.print('FPS '.. tostring(love.timer.getFPS()), 40, 20)
+    love.graphics.setColor(255 / 255, 255 / 255, 255 / 255, 255 / 255)
+end
