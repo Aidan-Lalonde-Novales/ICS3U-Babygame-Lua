febb83a2c7c35c0f7f8eff0419de90deb73d274d
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-06-23 04:18:29 +0000

    m

---
+++ Babygame/Babygame2/main.lua
@@ -0,0 +1,59 @@
+-- 3rd Baby Game Learning Guide
+
+-- adds a block sprite to act as the ground, and a background
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+function love.load()  
+    love.window.setTitle('Baby Game by A.M.W.L.N')
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')  
+    background = love.graphics.newImage("images/background.png")
+    block = love.graphics.newImage("images/block.png")
+
+    smallfont = love.graphics.newFont('fonts/pixel_text.ttf', 14)
+    largefont = love.graphics.newFont('fonts/pixel_text.ttf', 32)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true
+    })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.draw()
+    push:apply('start')
+
+    displayFPS()
+
+    love.graphics.draw(background)
+    love.graphics.draw(block)
+
+    love.graphics.setFont(largefont)
+    love.graphics.printf("Hello World!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    push:apply('end')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function displayFPS()
+    love.graphics.setColor(254 / 255, 255 / 255, 254 / 255, 255 / 255)
+    love.graphics.setFont(smallfont)
+    love.graphics.print('FPS '.. tostring(love.timer.getFPS()), 16, 8)
+    love.graphics.setColor(255 / 255, 255 / 255, 255 / 255, 255 / 255)
+end
